1dab567c65b2b11ab2eda1ac0d9465885050dcfc
asajeev <asajeev@opentrends.net>
2020-02-17 14:26:13 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into users (username,password,enabled) values ('akhil','akhil',true);
+insert into users (username,password,enabled) values ('minnu','minnu',true);
+
+insert into authorities (username,authority) values ('akhil','ROLE_ADMIN');
+insert into authorities (username,authority) values ('minnu','ROLE_USER');
+
